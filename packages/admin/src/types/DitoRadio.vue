08174e8c4e8441f12c07cbba7ca6efa147d3b705
wirsing <wirsingwirsing@googlemail.com>
2017-05-05 10:49:28 -0700

    Add radio button component and example.

---
+++ packages/admin/src/types/DitoRadio.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  ul.dito-radio
+    li(v-for="option in desc.options")
+      input(
+        :id="name",
+        :name="name",
+        type="radio",
+        v-model="data[name]",
+        v-validate="validations",
+        :disabled="disabled",
+        :readonly="readonly",
+        :value="option.value || option"
+      )
+      label
+        | {{option}}
+</template>
+
+<style lang="sass">
+  // Used both by DitoList and DitoPane
+  ul.dito-radio
+    display: table
+    border-spacing: 0.2em
+    padding-bottom: 0.5em
+    > li
+      display: table-row
+      > *
+        display: table-cell
+</style>
+
+<script>
+import DitoComponent from '@/DitoComponent'
+
+export default DitoComponent.register('radio')
+</script>
