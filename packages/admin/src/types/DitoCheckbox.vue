11ccb134eedb6004bcc80bca5950a85b5fdcfffd
Jürg Lehni <juerg@scratchdisk.com>
2017-05-06 18:21:09 +0200

    Fix issue with checkbox root element.

--- packages/admin/src/types/DitoCheckbox.vue
+++ packages/admin/src/types/DitoCheckbox.vue
@@ -1,14 +1,12 @@
 <template lang="pug">
-  // TODO get rid of root element here
-  div
-    input.dito-checkbox(
-      :id="name",
-      :name="name",
-      type="checkbox",
-      v-model="data[name]",
-      v-validate="validations",
-      :disabled="disabled"
-    )
+  input.dito-checkbox(
+    :id="name",
+    :name="name",
+    type="checkbox",
+    v-model="data[name]",
+    v-validate="validations",
+    :disabled="disabled"
+  )
 </template>
 
 <script>
