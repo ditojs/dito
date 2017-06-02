f9277bbc4852411935f60c6d42240b299a748280
wirsing <wirsingwirsing@googlemail.com>
2017-06-02 11:02:34 -0700

    Add Textarea type and example.

---
+++ packages/admin/src/types/DitoTextarea.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  textarea.dito-textarea.dito-input(
+    :id="name",
+    :name="name",
+    rows="8",
+    v-model="data[name]",
+    v-validate="validations",
+    :placeholder="placeholder",
+    :disabled="disabled",
+    :readonly="readonly"
+  )
+</template>
+
+<script>
+import DitoComponent from '@/DitoComponent'
+
+export default DitoComponent.register('textarea')
+</script>
