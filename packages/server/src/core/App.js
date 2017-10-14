7dcfd72d81984faa5934955fddb7cce17c93aeb5
Jürg Lehni <juerg@scratchdisk.com>
2017-10-14 18:01:10 +0200

    Improve app start / stop methods

--- packages/server/src/core/App.js
+++ packages/server/src/core/App.js
@@ -47,15 +47,34 @@ export default class App extends Koa {
       server: { host, port },
       environment
     } = this.config
-    this.server = this.listen(port, host, () => {
-      // TODO: logging?
-      console.log(
-        `${environment} server started at http://${host}:${port}`
-      )
+    return new Promise((resolve, reject) => {
+      this.server = this.listen(port, host, () => {
+        // TODO: logging?
+        console.log(
+          `${environment} server started at http://${host}:${port}`
+        )
+        resolve(this.server)
+      })
+      if (!this.server) {
+        resolve(new Error(`Unable to start server at http://${host}:${port}`))
+      }
     })
   }
 
   stop() {
-    // TODO: server.close()...
+    return new Promise((resolve, reject) => {
+      if (this.server) {
+        this.server.close(err => {
+          this.server = null
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        })
+      } else {
+        reject(new Error('Server is not running'))
+      }
+    })
   }
 }
