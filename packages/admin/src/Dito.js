e61f7b4b5bf56bd80175401b95f3d0861c621c0e
Jürg Lehni <juerg@scratchdisk.com>
2017-12-08 13:33:24 +0100

    Refactor schema processing
    Delegate processing to type components

--- packages/admin/src/Dito.js
+++ packages/admin/src/Dito.js
@@ -5,9 +5,8 @@ import './components'
 import './types'
 import TypeComponent from './TypeComponent'
 import DitoRoot from './components/DitoRoot'
-import DitoView from './components/DitoView'
-import DitoForm from './components/DitoForm'
-import { isFunction, isPromise, hyphenate } from './utils'
+import { isFunction, hyphenate } from './utils'
+import { processComponent } from './schema'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
@@ -18,10 +17,6 @@ Vue.use(VeeValidate, {
   errorBagName: '$errors',
   fieldsBagName: '$fields'
 })
-
-const user = {
-  role: 'admin' // TODO
-}
 
 export async function setup(el, options = {}) {
   const {
@@ -42,19 +37,19 @@ export async function setup(el, options = {}) {
       return `${component.listSchema.path}/${itemId}`
     },
     collection(component) {
-      const { parentFormComponent: parentForm, listSchema } = component
-      return parentForm
-        ? `${parentForm.listSchema.path}/${parentForm.itemId}/${listSchema.path}`
+      const { parentFormComponent: parent, listSchema } = component
+      return parent
+        ? `${parent.listSchema.path}/${parent.itemId}/${listSchema.path}`
         : listSchema.path
     },
     ...api.resources
   }
 
+  // Collect all routes from the root schema components
   const routes = []
   const promises = []
   for (const [name, schema] of Object.entries(schemas)) {
-    // TODO: Could be other things than lists in the future: add processSchema()
-    promises.push(processList(api, schema, name, routes, 0))
+    promises.push(processComponent(schema, name, api, routes, 0))
   }
   await Promise.all(promises)
 
@@ -71,121 +66,6 @@ export async function setup(el, options = {}) {
       settings
     }
   })
-}
-
-async function processList(api, listSchema, name, routes, level) {
-  let { form, forms } = listSchema
-  // Resolve the forms and store the results back on the schema.
-  if (forms) {
-    forms = listSchema.forms = await resolveForms(forms)
-  } else if (form) {
-    form = listSchema.form = await resolveForm(form)
-    forms = { default: form } // Only used for processForms() below.
-  }
-  if (!forms) return
-  const path = listSchema.path = listSchema.path || api.processPath(name)
-  listSchema.name = name
-  const { inline, nested } = listSchema
-  const addRoutes = !inline
-  if (inline) {
-    if (nested === false) {
-      throw new Error(
-        'Lists with inline forms can only work with nested data')
-    }
-    listSchema.nested = true
-  }
-  const root = level === 0
-  const childRoutes = forms && await processForms(api, forms, level)
-  if (addRoutes) {
-    // While root schemas have their own vue route objects, nested lists in
-    // forms don't have their own route objects and need their path prefixed.
-    const pathPrefix = root ? '' : `${path}/`
-    // Use differently named url parameters on each nested level for id as
-    // otherwise they would clash and override each other inside $route.params
-    // See: https://github.com/vuejs/vue-router/issues/1345
-    const param = `id${level + 1}`
-    const meta = {
-      user,
-      api,
-      listSchema
-    }
-    const formRoute = {
-      path: `${pathPrefix}:${param}`,
-      component: DitoForm,
-      children: childRoutes,
-      meta: {
-        ...meta,
-        param
-      }
-    }
-    routes.push(
-      root
-        ? {
-          path: `/${path}`,
-          ...(formRoute && {
-            children: [formRoute]
-          }),
-          component: DitoView,
-          meta: {
-            ...meta,
-            schema: listSchema
-          }
-        }
-        // Just redirect back to the form if the user hits a nested list route
-        : {
-          path,
-          redirect: '.'
-        },
-      // Include the prefixed formRoutes for nested lists.
-      ...(!root && formRoute && [formRoute])
-    )
-  }
-}
-
-async function processForms(api, forms, level) {
-  const children = []
-  const promises = []
-  function processComponents(components) {
-    for (const [name, component] of Object.entries(components || {})) {
-      promises.push(processList(api, component, name, children, level + 1))
-    }
-  }
-  for (const form of Object.values(forms)) {
-    for (const tab of Object.values(form.tabs || {})) {
-      processComponents(tab.components)
-    }
-    processComponents(form.components)
-  }
-  await Promise.all(promises)
-  return children
-}
-
-async function resolveForm(form) {
-  if (isFunction(form)) {
-    form = form()
-  }
-  if (isPromise(form)) {
-    form = await form
-  }
-  // When dynamically importing forms, try figuring out and setting their
-  // name, if they were declared as named imports:
-  if (form && !form.components) {
-    const name = Object.keys(form)[0]
-    form = form[name]
-    if (name !== 'default') {
-      form.name = name
-    }
-  }
-  return form
-}
-
-async function resolveForms(forms) {
-  // Basically Promise.props() without bluebird:
-  const results = await Promise.all(Object.values(forms).map(resolveForm))
-  return Object.keys(forms).reduce((mapped, key, index) => {
-    mapped[key] = results[index]
-    return mapped
-  }, {})
 }
 
 export const { register } = TypeComponent
---
+++ packages/admin/src/schema.js
@@ -0,0 +1,64 @@
+import TypeComponent from './TypeComponent'
+import { isFunction, isPromise } from './utils'
+
+export async function processComponent(schema, name, api, routes, level) {
+  // Delegate processing to the actual type components.
+  const component = TypeComponent.get(schema.type)
+  return component?.options.processSchema?.(schema, name, api, routes, level)
+}
+
+export async function processForms(schema, api, level) {
+  const children = []
+  const promises = []
+
+  function processComponents(components) {
+    for (const [name, component] of Object.entries(components || {})) {
+      promises.push(processComponent(component, name, api, children, level + 1))
+    }
+  }
+
+  // First resolve the forms and store the results back on the schema.
+  let { form, forms } = schema
+  if (forms) {
+    forms = schema.forms = await resolveForms(forms)
+  } else if (form) {
+    form = schema.form = await resolveForm(form)
+    forms = { default: form } // Only used for loop below.
+  }
+  for (const form of Object.values(forms)) {
+    for (const tab of Object.values(form.tabs || {})) {
+      processComponents(tab.components)
+    }
+    processComponents(form.components)
+  }
+  await Promise.all(promises)
+  return children
+}
+
+export async function resolveForm(form) {
+  if (isFunction(form)) {
+    form = form()
+  }
+  if (isPromise(form)) {
+    form = await form
+  }
+  // When dynamically importing forms, try figuring out and setting their
+  // name, if they were declared as named imports:
+  if (form && !form.components) {
+    const name = Object.keys(form)[0]
+    form = form[name]
+    if (name !== 'default') {
+      form.name = name
+    }
+  }
+  return form
+}
+
+export async function resolveForms(forms) {
+  // Basically Promise.props() without bluebird:
+  const results = await Promise.all(Object.values(forms).map(resolveForm))
+  return Object.keys(forms).reduce((mapped, key, index) => {
+    mapped[key] = results[index]
+    return mapped
+  }, {})
+}
