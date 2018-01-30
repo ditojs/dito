cd148773384bbb7d38ab9c5fff2c402ba0995ae0
Jürg Lehni <juerg@scratchdisk.com>
2018-01-30 21:35:53 +0100

    Improve error handling in seed()

--- packages/server/src/cli/db/seed.js
+++ packages/server/src/cli/db/seed.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import fs from 'fs-extra'
 import chalk from 'chalk'
+import util from 'util'
 import { isFunction, isArray } from '@ditojs/utils'
 
 export async function seed(app) {
@@ -9,24 +10,35 @@ export async function seed(app) {
   for (const file of files) {
     const { name, ext, base } = path.parse(file)
     if (/^\.(js|json)$/.test(ext)) {
-      const object = await import(path.resolve(seedDir, file))
-      const seed = object.default || object
-      let res
-      if (isFunction(seed)) {
-        res = await seed(app.models)
-      } else {
-        const modelClass = app.models[name]
-        if (modelClass) {
-          await modelClass.truncate()
-          res = await modelClass.insertGraph(seed)
+      try {
+        const object = await import(path.resolve(seedDir, file))
+        const seed = object.default || object
+        let res
+        if (isFunction(seed)) {
+          res = await seed(app.models)
+        } else {
+          const modelClass = app.models[name]
+          if (modelClass) {
+            await modelClass.truncate()
+            res = await modelClass.insertGraph(seed)
+          }
         }
-      }
-      if (isArray(res)) {
-        console.log(chalk.green(
-          `${base}: ${res.length} seed records created.`))
-      } else {
-        console.log(chalk.red(
-          `${base}: No seed records created.`))
+        if (isArray(res)) {
+          console.log(chalk.green(
+            `${base}: ${res.length} seed records created.`))
+        } else {
+          console.log(chalk.red(
+            `${base}: No seed records created.`))
+        }
+      } catch (err) {
+        console.error(
+          chalk.red(`${base}:`),
+          util.inspect(err, {
+            colors: true,
+            depth: null,
+            maxArrayLength: null
+          })
+        )
       }
     }
   }
