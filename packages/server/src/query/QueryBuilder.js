8507e9778d3da802139006b2a971681f8caa7053
Jürg Lehni <juerg@scratchdisk.com>
2018-02-09 17:12:15 +0100

    Fix double application in applyScope(scope, true)
    And remove all workarounds added to handle its side-effects

--- packages/server/src/query/QueryBuilder.js
+++ packages/server/src/query/QueryBuilder.js
@@ -16,8 +16,6 @@ export class QueryBuilder extends objection.QueryBuilder {
     super(modelClass)
     this._propertyRefsAllowed = null
     this._propertyRefsCache = {}
-    this._joinedRelations = {}
-    this._rawSelects = {}
     this._eagerScopeId = 0
     this.clearScope(true)
   }
@@ -26,8 +24,6 @@ export class QueryBuilder extends objection.QueryBuilder {
     const copy = super.clone()
     copy._propertyRefsAllowed = this._propertyRefsAllowed
     copy._propertyRefsCache = this._propertyRefsCache
-    copy._joinedRelations = { ...this._joinedRelations }
-    copy._rawSelects = { ...this._rawSelects }
     copy._scopes = clone(this._scopes)
     return copy
   }
@@ -63,17 +59,19 @@ export class QueryBuilder extends objection.QueryBuilder {
       if (this._eagerExpression) {
         const name = `_eagerScope${++this._eagerScopeId}_`
         const filters = {
-          ...this._eagerFilters,
           [name]: query => query.applyScope(scope, eager)
         }
         this.eager(
           addEagerScope(
             this.modelClass(),
             this._eagerExpression,
-            [scope, name],
+            [name],
             filters
           ),
-          filters
+          {
+            ...this._eagerFilters,
+            ...filters
+          }
         )
       }
     } else {
@@ -129,15 +127,7 @@ export class QueryBuilder extends objection.QueryBuilder {
   }
 
   selectRaw(...args) {
-    const raw = this.raw(...args)
-    // Prevent multiple calls of selectRaw() with the same statement from being
-    // executed multiple times, e.g. through named filters chaining.
-    const key = raw.toString().trim()
-    if (!this._rawSelects[key]) {
-      this._rawSelects[key] = true
-      this.select(raw)
-    }
-    return this
+    return this.select(this.raw(...args))
   }
 
   async truncate() {
@@ -463,32 +453,6 @@ for (const key of [
       args[i] = convertArgument(args[i])
     }
     return method.call(this, ...args)
-  }
-}
-
-// Allow all joinRelation() operations to be called multiple times for the same
-// relation, as long as the same operation is being executed with the same args,
-// but only execute the first time it's encountered to prevent SQL errors.
-// This is to allow more flexible combinations of scopes and eager statements.
-for (const key of [
-  'joinRelation', 'innerJoinRelation', 'outerJoinRelation', 'leftJoinRelation',
-  'leftOuterJoinRelation', 'rightJoinRelation', 'rightOuterJoinRelation',
-  'fullOuterJoinRelation'
-]) {
-  const method = QueryBuilder.prototype[key]
-  QueryBuilder.prototype[key] = function (expr, ...args) {
-    const identifier = `${expr} ${JSON.stringify(args)}`
-    const joined = this._joinedRelations[identifier]
-    if (joined === key) {
-      return this
-    } else if (joined) {
-      throw new QueryBuilderError(
-        `Unable to execute different join operations for the same relation ` +
-        `expression ${expr} within the same query`
-      )
-    }
-    this._joinedRelations[identifier] = key
-    return method.call(this, expr, ...args)
   }
 }
 
