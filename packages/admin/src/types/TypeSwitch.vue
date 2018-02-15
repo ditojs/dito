b3786e82b71fbec7fb2c6073b4ddaefa1817e1bd
Jürg Lehni <juerg@scratchdisk.com>
2018-02-15 17:15:10 +0100

    TypeSwitch: Improve layout & styling

--- packages/admin/src/types/TypeSwitch.vue
+++ packages/admin/src/types/TypeSwitch.vue
@@ -17,13 +17,24 @@
 
 <style lang="sass">
   .dito-switch
+    display: inline-block
+    vertical-align: baseline
+    // TODO: Fix vue-js-toggle-button to allow more flexible style overrides
+    font-size: inherit !important // override vue-js-toggle-button
+    height: 2em
     .v-switch-core
+      position: relative !important // override vue-js-toggle-button
       background-color: $color-light
+      top: 50%
+      transform: translateY(-50%)
     &.toggled
       .v-switch-core
         background-color: $color-active
     .v-switch-label
+      font-size: 0.8em
       text-transform: uppercase
+      top: 50% !important // override vue-js-toggle-button
+      transform: translateY(-50%)
 </style>
 
 <script>
