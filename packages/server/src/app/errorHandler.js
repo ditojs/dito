1cab2141a61309afa6308699167a6fe3d8cf48fc
Jürg Lehni <juerg@scratchdisk.com>
2018-03-24 15:23:12 +0100

    Don't send json errors back for js requests

--- packages/server/src/app/errorHandler.js
+++ packages/server/src/app/errorHandler.js
@@ -6,11 +6,13 @@ export default function errorHandler() {
       await next()
     } catch (err) {
       ctx.status = err.status || 500
-      ctx.body = err instanceof ResponseError
-        ? err.toJSON()
-        : {
-          message: err.message || 'An error has occurred.'
-        }
+      if (ctx.accepts('json') && !ctx.request.URL.pathname.endsWith('.js')) {
+        ctx.body = err instanceof ResponseError
+          ? err.toJSON()
+          : {
+            message: err.message || 'An error has occurred.'
+          }
+      }
       ctx.app.emit('error', err, ctx)
     }
   }
