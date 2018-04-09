167f21e29039ad0f4807a08028e5d8ae0417dae1
Jürg Lehni <juerg@scratchdisk.com>
2018-04-09 15:04:11 +0200

    Simplify isAbsoluteUrl() regexp

--- packages/utils/src/url.js
+++ packages/utils/src/url.js
@@ -3,5 +3,5 @@ export function isAbsoluteUrl(url) {
   // (protocol-relative URL).  RFC 3986 defines scheme name as a sequence of
   // characters beginning with a letter and followed by any combination of
   // letters, digits, plus, period, or hyphen.
-  return /^([a-z][a-z\d\+\-\.]*:)?\/\//i.test(url)
+  return /^([a-z][a-z\d+-.]*:)?\/\//i.test(url)
 }
