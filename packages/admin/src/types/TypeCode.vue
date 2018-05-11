30c5ed8c184c577cd0b487e1fff0851282033946
Jürg Lehni <juerg@scratchdisk.com>
2018-05-11 14:07:33 +0200

    Update CodeFlask to latest version

--- packages/admin/src/types/TypeCode.vue
+++ packages/admin/src/types/TypeCode.vue
@@ -1,42 +1,39 @@
 <template lang="pug">
-  .dito-code.CodeFlask(
+  .dito-code(
     ref="code"
     :style="style"
   ) {{ value }}
 </template>
 
 <style lang="sass">
-@import '~prismjs/themes/prism.css'
-@import '~codeflask/src/codeflask.css'
-
 .dito
   .dito-code
+    @extend %input
+    position: relative
+    // For propper sizing of content along sith :style="style" setting above,
+    // for proper line-height calculation.
     padding: $input-padding
-
-    .CodeFlask__textarea,
-    .CodeFlask__pre
-      height: 100%
-      &,
-      code,
-      pre
-        font-family: $font-family-mono
-        font-size: $font-size
-        line-height: $line-height
-        padding: $input-padding !important
-
-    .CodeFlask__textarea
-      @extend %input
-      opacity: 1
-
-    .CodeFlask__pre
-      border: $border-width solid transparent // align with .CodeFlask__textarea
-      bottom: 0 // for scroll to work!
-      .CodeFlask__code,
-      .token
-        background: transparent
-        text-shadow: none
     &.dito-fill
       width: auto
+    .codeflask
+      background: none
+      // Ignore theparent padding defined above which is only needed to set
+      // the desired height with :style="style".
+      top: 0
+      left: 0
+    .codeflask__textarea,
+    .codeflask__pre
+      // Use same padding as .dito-code
+      padding: $input-padding
+    .codeflask__textarea,
+    .codeflask__code,
+    .codeflask__lines
+      font-family: $font-family-mono
+      font-size: $font-size
+      line-height: $line-height
+    .codeflask__lines
+      padding: $input-padding
+    .codeflask__lines__line
 </style>
 
 <script>
@@ -45,13 +42,16 @@ import CodeFlask from 'codeflask'
 
 export default TypeComponent.register('code', {
   mounted() {
-    const indent = Array((this.schema.indentSize || 2) + 1).join(' ')
-    const flask = new CodeFlask(indent)
-    flask.run(this.$refs.code, {
-      language: this.schema.language || 'javascript'
+    const flask = new CodeFlask(this.$refs.code, {
+      language: this.schema.language || 'javascript',
+      tabSize: this.schema.indentSize || 2,
+      lineNumbers: false
     })
-    flask.onUpdate(value => { this.value = value })
-    this.$watch('value', value => { flask.update(value) })
+    flask.onUpdate(value => {
+      this.value = value
+      console.log(value)
+    })
+    this.$watch('value', value => { flask.updateCode(value) })
   },
 
   computed: {
