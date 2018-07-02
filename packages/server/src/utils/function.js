2926bdd154706221209b246d5c23b23be63ed502
Jürg Lehni <juerg@scratchdisk.com>
2018-07-02 21:19:49 +0200

    Add unit tests for server function utils

--- packages/server/src/utils/function.js
+++ packages/server/src/utils/function.js
@@ -2,13 +2,13 @@ export function describeFunction(func) {
   // A helper that describes a method by turning it int a short version without
   // printing its body.
   const match = func.toString().match(
-    /^\s*(?:function[^(]*\(([^)]*)\)|\(([^)]*)\)\s*=>|(\S*)\s*=>)\s*(.)/
+    /^\s*(async\s*|)(?:function[^(]*\(([^)]*)\)|\(([^)]*)\)\s*=>|(\S*)\s*=>)\s*(.)/
   )
   if (match) {
-    const body = match[4] === '{' ? '{ ... }' : '...'
-    return match[1] !== undefined ? `function (${match[1]}) ${body}`
-      : match[2] !== undefined ? `(${match[2]}) => ${body}`
-      : match[3] !== undefined ? `${match[3]} => ${body}`
+    const body = match[5] === '{' ? '{ ... }' : '...'
+    return match[2] !== undefined ? `${match[1]}function (${match[2]}) ${body}`
+      : match[3] !== undefined ? `${match[1]}(${match[3]}) => ${body}`
+      : match[4] !== undefined ? `${match[1]}${match[4]} => ${body}`
       : ''
   }
 }
---
+++ packages/server/src/utils/function.test.js
@@ -0,0 +1,53 @@
+import { describeFunction } from './function'
+
+describe('describeFunction()', () => {
+  it('describes normal functions', () => {
+    expect(describeFunction(function(a, b, c) { return a + b + c }))
+      .toBe('function (a, b, c) { ... }')
+  })
+
+  it('describes lambdas with one param and a body', () => {
+    expect(describeFunction(a => { return a }))
+      .toBe('a => { ... }')
+  })
+
+  it('describes lambdas with one param and no body', () => {
+    expect(describeFunction(a => a))
+      .toBe('a => ...')
+  })
+
+  it('describes lambdas with multiple params and a body', () => {
+    expect(describeFunction((a, b, c) => { return a + b + c }))
+      .toBe('(a, b, c) => { ... }')
+  })
+
+  it('describes lambdas with multiple params and no body', () => {
+    expect(describeFunction((a, b, c) => a + b + c))
+      .toBe('(a, b, c) => ...')
+  })
+
+  it('describes async functions', () => {
+    expect(describeFunction(async function(a, b, c) { return a + b + c }))
+      .toBe('async function (a, b, c) { ... }')
+  })
+
+  it('describes lambdas with one param and a body', () => {
+    expect(describeFunction(async a => { return a }))
+      .toBe('async a => { ... }')
+  })
+
+  it('describes lambdas with one param and no body', () => {
+    expect(describeFunction(async a => a))
+      .toBe('async a => ...')
+  })
+
+  it('describes lambdas with multiple params and a body', () => {
+    expect(describeFunction(async (a, b, c) => { return a + b + c }))
+      .toBe('async (a, b, c) => { ... }')
+  })
+
+  it('describes lambdas with multiple params and no body', () => {
+    expect(describeFunction(async (a, b, c) => a + b + c))
+      .toBe('async (a, b, c) => ...')
+  })
+})
