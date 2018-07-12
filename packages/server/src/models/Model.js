1b91698dd6156a92ef91dece974ce2ed70552f81
Jürg Lehni <juerg@scratchdisk.com>
2018-07-13 00:50:25 +0200

    Implement Model.populateGraph(graph, expression)
    As a way to populate existing graphs with relation expressions

--- packages/server/src/models/Model.js
+++ packages/server/src/models/Model.js
@@ -105,8 +105,8 @@ export class Model extends objection.Model {
   static isReference(data) {
     let validator = this.referenceValidator
     if (!validator) {
-      const idName = this.getIdProperty()
-      const { type } = this.definition.properties[idName]
+      const idProperty = this.getIdProperty()
+      const { type } = this.definition.properties[idProperty]
       // For `data` to be considered a reference, it needs to hold only one
       // value that is either the target's id, or an Objection.js #ref value:
       validator = this.referenceValidator = this.app.compileValidator({
@@ -114,7 +114,7 @@ export class Model extends objection.Model {
           {
             type: 'object',
             properties: {
-              [idName]: {
+              [idProperty]: {
                 type
               }
             },
@@ -382,6 +382,131 @@ export class Model extends objection.Model {
       delete json[key]
     }
     return json
+  }
+
+  static async populateGraph(graph, expr) {
+    expr = objection.RelationExpression.create(expr)
+    // Convert RelationExpression to an array of paths, that themselves contain
+    // path entries with relation names and modify settings.
+    const collectPaths = expr => {
+      const paths = []
+      for (const key in expr) {
+        if (expr.hasOwnProperty(key)) {
+          const child = expr[key]
+          if (isObject(child)) {
+            const entry = {
+              relation: child.$relation || key,
+              modify: child.$modify
+            }
+            const subPaths = collectPaths(child)
+            if (subPaths.length > 0) {
+              // The child has itself children.
+              for (const subPath of subPaths) {
+                paths.push([entry, ...subPath])
+              }
+            } else {
+              // The child is a leaf, add $modify
+              paths.push([entry])
+            }
+          }
+        }
+      }
+      return paths
+    }
+
+    const grouped = {}
+    const addToGroup = (item, modelClass, modify, eager) => {
+      // Group models by model-name + modify + eager, for faster loading:
+      const key = `${modelClass.name}_${modify}_${eager || ''}`
+      const group = grouped[key] || (grouped[key] = {
+        modelClass,
+        modify,
+        eager,
+        idProperty: modelClass.getIdProperty(),
+        references: [],
+        ids: [],
+        modelsById: {}
+      })
+      // Filter out the items that aren't references,
+      // and collect ids to be loaded for references.
+      if (modify.length > 0 || modelClass.isReference(item)) {
+        group.references.push(item)
+        group.ids.push(item[group.idProperty])
+      }
+    }
+
+    for (const path of collectPaths(expr)) {
+      let modelClass = this
+      const modelClasses = []
+      let modify
+      for (const entry of path) {
+        modelClasses.push(modelClass)
+        modelClass = modelClass.getRelation(entry.relation).relatedModelClass
+        modify = entry.modify
+      }
+      for (const model of asArray(graph)) {
+        if (model) {
+          let items = asArray(model)
+          // Collect all graph items described by the current relation path in
+          // one loop:
+          for (let i = 0, l = path.length; i < l; i++) {
+            const entry = path[i]
+            if (items.length === 0) break
+            const modelClass = modelClasses[i]
+            items = items.reduce((items, item) => {
+              if (modelClass.isReference(item)) {
+                // Detected a reference item that isn't a leave: We need to
+                // eager-load the rest of the path:
+                const eager = path.slice(i).map(({ relation }) => relation)
+                addToGroup(item, modelClass, entry.modify, eager.join('.'))
+              } else {
+                const value = item[entry.relation]
+                if (value != null) {
+                  items.push(...asArray(value))
+                }
+              }
+              return items
+            }, [])
+          }
+          // Add all encountered leaf-references to groups to be loaded.
+          for (const item of items) {
+            addToGroup(item, modelClass, modify)
+          }
+        }
+      }
+    }
+
+    const groups = Object.values(grouped)
+
+    // Load all found models by ids asynchronously.
+    await Promise.map(
+      groups,
+      async ({ modelClass, modify, eager, idProperty, ids, modelsById }) => {
+        const query = modelClass.whereIn('id', ids)
+        if (eager) {
+          query.mergeEager(eager)
+        }
+        for (const mod of modify) {
+          query.modify(mod)
+        }
+        const models = await query.execute()
+        // Fill the group.modelsById lookup:
+        for (const model of models) {
+          modelsById[model[idProperty]] = model
+        }
+      }
+    )
+
+    // Finally populate the references with the loaded models.
+    for (const { idProperty, references, modelsById } of groups) {
+      for (const item of references) {
+        const id = item[idProperty]
+        const model = modelsById[id]
+        if (model) {
+          Object.assign(item, model)
+        }
+      }
+    }
   }
 
   static createNotFoundError(ctx) {
