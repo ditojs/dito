27fa11d1d181cb177d8777931cbcca506fba525a
Jürg Lehni <juerg@scratchdisk.com>
2018-09-30 19:52:58 +0200

    Implement array shuffle() with tests

--- packages/utils/src/array.js
+++ packages/utils/src/array.js
@@ -12,3 +12,15 @@ export function flatten(array, maxDepth = Infinity, _depth = 0) {
   }
   return res
 }
+
+export function shuffle(array) {
+  // Do the Fisher-Yates (aka Knuth) Shuffle:
+  const res = array.slice()
+  for (let i = array.length; i;) {
+    const r = Math.floor(Math.random() * i)
+    const t = res[--i]
+    res[i] = res[r]
+    res[r] = t
+  }
+  return res
+}
