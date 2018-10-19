54df55a9f4a3de30b4a1a16d13c0f4b27c16ddb3
Jürg Lehni <juerg@scratchdisk.com>
2018-10-20 01:09:46 +0200

    Change handleError() to clear ctx.body on error

--- packages/server/src/app/middleware/handleError.js
+++ packages/server/src/app/middleware/handleError.js
@@ -6,12 +6,19 @@ export function handleError() {
       await next()
     } catch (err) {
       ctx.status = err.status || 500
-      if (ctx.accepts('json') && !ctx.request.URL.pathname.endsWith('.js')) {
+      if (ctx.accepts('json') && !ctx.request.path.endsWith('.js')) {
+        // Format error as JSON
         ctx.body = err instanceof ResponseError
           ? err.toJSON()
           : {
             message: err.message || 'An error has occurred.'
           }
+      } else {
+        // TODO: Consider handling html and xml responses also, see:
+        // https://github.com/strongloop/strong-error-handler/blob/master/lib/send-html.js
+        // https://github.com/strongloop/strong-error-handler/blob/master/lib/send-xml.js
+        // https://github.com/strongloop/strong-error-handler/blob/master/views/default-error.ejs
+        ctx.body = null
       }
       ctx.app.emit('error', err, ctx)
     }
