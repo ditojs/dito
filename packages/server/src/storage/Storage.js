f61e334d5012603958da369965d2b21eb68c7526
Jürg Lehni <juerg@scratchdisk.com>
2019-04-05 17:38:53 +0200

    Introduce `readImageSize` config to control image size detection in uploads
    Also refactor handling of config in asset uploads and how they get passed on to the upload handlers

--- packages/server/src/storage/Storage.js
+++ packages/server/src/storage/Storage.js
@@ -18,70 +18,87 @@ export class Storage {
 
   setStorage(storage) {
     this.storage = storage
-    // Monkey-patch storage._handleFile to also read image size from the stream
-    // and set the file fields accordingly.
-    const { _handleFile } = storage
-    storage._handleFile = async (req, file, callback) => {
-      if (this.isImageFile(file)) {
-        // Handle image size detection in parallel with multer file upload:
-        try {
-          // Create two readable clones of the stream that can be read from
-          // simultaneously: The first to override `file.stream` with,
-          // the second to run the image size detection on:
-          const { stream } = file
-          // Pause the original stream until the original _handleFile() is
-          // called, to preserve original stream event behavior.
-          stream.pause()
-          const stream1 = new ReadableClone(stream)
-          const stream2 = new ReadableClone(stream)
-          Object.defineProperty(file, 'stream', {
-            configurable: true,
-            enumerable: false,
-            value: stream1
-          })
-          const [res, size] = await Promise.all([
-            // The original `_handleFile()`:
-            new Promise((resolve, reject) => {
-              stream.resume()
-              _handleFile.call(
-                storage, req, file,
-                toPromiseCallback(resolve, reject)
-              )
-            }).finally(() => stream1.destroy()),
-            // Image size detection:
-            new Promise(resolve => {
-              stream2
-                .pipe(new ImageSizeTransform())
-                .on('size', resolve)
-                .on('error', err => {
-                  // Do not reject with this error, but log it:
-                  this.app.emit(
-                    'error',
-                    `Unable to determine image size: ${err}`
-                  )
-                  resolve(null)
-                })
-            }).finally(() => stream2.destroy())
-          ])
-          if (size) {
-            const { width, height } = size
-            res.width = width
-            res.height = height
-          }
-          callback(null, res)
-        } catch (err) {
-          callback(err)
-        }
-      } else {
-        return _handleFile.call(storage, req, file, callback)
+  }
+
+  async handleUpload(req, file, config) {
+    if (config.readImageSize && this.isImageFile(file)) {
+      // Handle image size detection in parallel with multer file upload:
+      // Create two readable clones of the stream that can be read from
+      // simultaneously: The first to override `file.stream` with,
+      // the second to run the image size detection on.
+      // Pause the original stream until the original `storage._handleFile()` is
+      // called, to preserve original stream event behavior.
+      const { stream } = file
+      stream.pause() // resume() happens in this.handleFile()
+      const stream1 = new ReadableClone(stream)
+      const stream2 = new ReadableClone(stream)
+      const [res, size] = await Promise.all([
+        this.handleFile(req, file, stream1).finally(() => stream1.destroy()),
+        this.getImageSize(stream2).finally(() => stream2.destroy())
+      ])
+      if (size) {
+        const { width, height } = size
+        res.width = width
+        res.height = height
       }
+      return res
+    } else {
+      return this.handleFile(req, file)
     }
   }
 
-  getUploadHandler(config) {
+  handleFile(req, file, stream = null) {
+    // Calls the original `storage._handleFile()`, wrapped in a promise:
+    return new Promise((resolve, reject) => {
+      if (stream) {
+        // If there is a ReadableClone stream to read from instead, resume the
+        // original stream now and replace `file.stream` with the clone:
+        file.stream.resume()
+        Object.defineProperty(file, 'stream', {
+          configurable: true,
+          enumerable: false,
+          value: stream
+        })
+      }
+      this.storage._handleFile(req, file, toPromiseCallback(resolve, reject))
+    })
+  }
+
+  getImageSize(stream) {
+    return new Promise(resolve => {
+      stream
+        .pipe(new ImageSizeTransform())
+        .on('size', resolve)
+        .on('error', err => {
+          // Do not reject with this error, but log it:
+          this.app.emit(
+            'error',
+            `Unable to determine image size: ${err}`
+          )
+          resolve(null)
+        })
+    })
+  }
+
+  getUploadStorage(config) {
+    // Returns a storage that inherits from this.storage but overrides
+    // _handleFile to pass on `settings` to the call of `handleUpload()`
     return this.storage
-      ? multer({ ...config, storage: this.storage }).any()
+      ? Object.setPrototypeOf({
+        _handleFile: async (req, file, callback) => {
+          try {
+            callback(null, await this.handleUpload(req, file, config))
+          } catch (err) {
+            callback(err)
+          }
+        }
+      }, this.storage)
       : null
+  }
+
+  getUploadHandler(config) {
+    const storage = this.getUploadStorage(config)
+    return storage ? multer({ ...config, storage }).any() : null
   }
 
   getFilename(file) {
