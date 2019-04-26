0b4a9721e7f5b7ef69b23f36b6f5f037dcb4806e
Jürg Lehni <juerg@scratchdisk.com>
2019-04-26 18:54:14 +0200

    Expose OrderedMixin.updateOrder()

--- packages/admin/src/mixins/OrderedMixin.js
+++ packages/admin/src/mixins/OrderedMixin.js
@@ -13,22 +13,30 @@ export default {
 
     onEndDrag(event, list = this.value, schema = this.schema) {
       this.dragging = false
+      const start = Math.min(event.oldIndex, event.newIndex)
+      this.updateOrder(list, schema, start)
+    },
+
+    updateOrder(list = this.value, schema = this.schema, start = 0) {
       const {
         draggable: {
           order
         } = {}
       } = schema
       if (order) {
-        // Reorder the changed children by their order key.
-        let start = Math.min(event.oldIndex, event.newIndex)
+        // Reorder the changed entries by their order key.
+        // TODO: Make this work with paginated lists, where we need to offest
+        // by the start index.
+        const offset = 0
         // Detect if no order values were set, and change `start` accordingly.
         while (start > 0 && list[start][order] == null) {
           start--
         }
         for (let i = start; i < list.length; i++) {
-          list[i][order] = i
+          list[i][order] = i + offset
         }
       }
+      return list
     }
   }
 }
