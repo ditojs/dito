9422cdda34673bf270c37388a662b13671966833
Jürg Lehni <juerg@scratchdisk.com>
2019-07-03 13:09:04 +0200

    Merge .dito-buttons & .dito-buttons-container

--- packages/admin/src/components/DitoButtons.vue
+++ packages/admin/src/components/DitoButtons.vue
@@ -1,37 +1,27 @@
 <template lang="pug">
+  // NOTE: This is similar to DitoComponents, but uses the DitoButtonContainer
+  // sub-class as the component container for different layout:
   .dito-buttons(
     v-if="buttonSchemas || $slots.default"
   )
-    // NOTE: This is similar to DitoComponents, but uses the DitoButtonContainer
-    // sub-class as the component container for different layout:
-    .dito-buttons-container
-      dito-button-container(
-        v-for="(buttonSchema, buttonDataPath) in buttonSchemas"
-        v-if="shouldRender(buttonSchema)"
-        :key="buttonDataPath"
-        :schema="buttonSchema"
-        :dataPath="buttonDataPath"
-        :data="data"
-        :meta="meta"
-        :store="getChildStore(buttonSchema.name)"
-        :disabled="disabled"
-        :generateLabels="false"
-      )
-      .dito-button-container(
-        v-for="node in $slots.default"
-        v-if="node.tag"
-      )
-        dito-vnode(:node="node")
+    dito-button-container(
+      v-for="(buttonSchema, buttonDataPath) in buttonSchemas"
+      v-if="shouldRender(buttonSchema)"
+      :key="buttonDataPath"
+      :schema="buttonSchema"
+      :dataPath="buttonDataPath"
+      :data="data"
+      :meta="meta"
+      :store="getChildStore(buttonSchema.name)"
+      :disabled="disabled"
+      :generateLabels="false"
+    )
+    .dito-button-container(
+      v-for="node in $slots.default"
+      v-if="node.tag"
+    )
+      dito-vnode(:node="node")
 </template>
-
-<style lang="sass">
-  .dito-buttons-container
-    display: flex
-    flex-flow: row wrap
-    flex: 100%
-    align-items: center
-    justify-content: flex-end
-</style>
 
 <script>
 import DitoComponent from '@/DitoComponent'
