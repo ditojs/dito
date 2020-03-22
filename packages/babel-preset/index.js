6e3adfffe187b268ef3d094e116a7f8eac2a83dc
Jürg Lehni <juerg@scratchdisk.com>
2020-03-22 11:25:17 +0100

    Update babel-preset stages

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -3,6 +3,23 @@ module.exports = function() {
     plugins: [
       // Stage 1
       require('@babel/plugin-proposal-export-default-from'),
+
+      // Stage 2
+      [
+        require('@babel/plugin-proposal-decorators'),
+        { legacy: true }
+      ],
+
+      // Stage 3
+      [
+        require('@babel/plugin-proposal-class-properties'),
+        { loose: false }
+      ],
+
+      // Stage 4
+      require('@babel/plugin-syntax-dynamic-import'),
+      require('@babel/plugin-proposal-export-namespace-from'),
+      require('@babel/plugin-proposal-optional-catch-binding'),
       [
         require('@babel/plugin-proposal-optional-chaining'),
         { loose: false }
@@ -10,24 +27,7 @@ module.exports = function() {
       [
         require('@babel/plugin-proposal-nullish-coalescing-operator'),
         { loose: false }
-      ],
-
-      // Stage 2
-      [
-        require('@babel/plugin-proposal-decorators'),
-        { legacy: true }
-      ],
-      require('@babel/plugin-proposal-export-namespace-from'),
-
-      // Stage 3
-      require('@babel/plugin-syntax-dynamic-import'),
-      [
-        require('@babel/plugin-proposal-class-properties'),
-        { loose: false }
-      ],
-
-      // Stage 4
-      require('@babel/plugin-proposal-optional-catch-binding')
+      ]
     ]
   }
 }
