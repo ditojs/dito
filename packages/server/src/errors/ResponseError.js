e17f19895ee13b6a54f180c44e7e314cb6651557
Jürg Lehni <juerg@scratchdisk.com>
2021-01-20 17:24:54 +0100

    Prevent leaking of axios configuration in errors

--- packages/server/src/errors/ResponseError.js
+++ packages/server/src/errors/ResponseError.js
@@ -5,12 +5,17 @@ export class ResponseError extends Error {
     const object = isPlainObject(error)
       ? error
       : error instanceof Error
-        ? { // Copy error into object so they can be merged with defaults after.
-          // First copy everything that is enumerable:
-          ...error,
-          // Also explicitly copy message and status.
+        ? {
+          // Copy error into object so they can be merged with defaults after.
+          // First copy everything that is enumerable, unless the error is from
+          // axios, in which case we don't want to leak config information.
+          // TODO: Use `instanceof AxiosException` instead once axios is v1.0.0:
+          // https://github.com/axios/axios/pull/2014
+          ...(error.isAxiosError ? null : error),
+          // Also explicitly copy message, status and code.
           message: error.message,
-          status: error.status
+          status: error.status,
+          code: error.code
         }
         : isString(error)
           ? { message: error }
