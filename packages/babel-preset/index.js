8ebc88b0c06a301788e7e033232c5d4a9bdba5b4
Jürg Lehni <juerg@scratchdisk.com>
2022-03-03 18:19:34 +0100

    Update all trivial dependencies

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -20,6 +20,7 @@ module.exports = function(api, { loose = false } = {}) {
       require('@babel/plugin-syntax-dynamic-import'),
       require('@babel/plugin-proposal-export-namespace-from'),
       require('@babel/plugin-proposal-optional-catch-binding'),
+      require('@babel/plugin-proposal-logical-assignment-operators'),
       [
         require('@babel/plugin-proposal-optional-chaining'),
         { loose }
