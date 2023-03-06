9f4d9e0519aeb0a74dbb6b25b0f90a4ad42b34da
Jürg Lehni <juerg@scratchdisk.com>
2023-03-06 19:58:03 +0100

    Some minor mapConcurrently() refactoring

--- packages/utils/src/promise/mapConcurrently.js
+++ packages/utils/src/promise/mapConcurrently.js
@@ -6,27 +6,28 @@ export async function mapConcurrently(
   const array = await input
   if (!concurrency) {
     return Promise.all(array.map(callback))
-  } else {
-    const promises = []
-    const results = []
-    let index = 0
-
-    function next() {
-      if (index < array.length) {
-        const i = index++
-        return Promise.resolve(callback(array[i], i, array)).then(result => {
-          results[i] = result
-          return next()
-        })
-      }
-    }
-
-    while (concurrency-- > 0) {
-      const promise = next()
-      if (!promise) break
-      promises.push(promise)
-    }
-
-    return Promise.all(promises).then(() => results)
   }
+
+  const promises = []
+  const results = []
+  let index = 0
+
+  function next() {
+    if (index < array.length) {
+      const i = index++
+      return Promise.resolve(callback(array[i], i, array)).then(result => {
+        results[i] = result
+        return next()
+      })
+    }
+    return null
+  }
+
+  while (concurrency-- > 0) {
+    const promise = next()
+    if (!promise) break
+    promises.push(promise)
+  }
+
+  return Promise.all(promises).then(() => results)
 }
