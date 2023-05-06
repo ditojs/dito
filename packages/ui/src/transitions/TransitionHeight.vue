a9d63f1a639bec393d34ee97631d62923580c2b2
Jürg Lehni <juerg@scratchdisk.com>
2023-05-06 09:29:43 +0200

    Make height transition respect `&--label-vertical` calculations

--- packages/ui/src/transitions/TransitionHeight.vue
+++ packages/ui/src/transitions/TransitionHeight.vue
@@ -16,43 +16,59 @@ export default TransitionHeight
 
 const setStyle = (element, style) => Object.assign(element.style, style)
 
+const nextTimeout = callback => setTimeout(callback, 0)
+
 // Force repaint to make sure the animation is triggered correctly.
 const forceRepaint = element => getComputedStyle(element).height
+
+const enterMap = new WeakMap()
 
 const props = {
   name: 'height',
 
   onAfterEnter(element) {
+    enterMap.delete(element)
     // A timeout before setting style is only really needed after calls to
     // `forceRepaint()`, but using one here too preserves execution sequence so
     // that the call from `enter()` can never outrun the one from `afterEnter()`
-    setTimeout(() => setStyle(element, { height: null }))
+    nextTimeout(() => setStyle(element, { height: null }))
   },
 
   onEnter(element) {
-    const { width } = getComputedStyle(element)
-    setStyle(element, {
-      width,
-      position: 'absolute',
-      visibility: 'hidden',
-      height: 'auto'
-    })
-    const { height } = getComputedStyle(element)
-    setStyle(element, {
-      width: null,
-      position: null,
-      visibility: null,
-      height: 0
-    })
-    forceRepaint(element)
-    setTimeout(() => setStyle(element, { height }))
+    // For some reason we're getting two `onEnter()` calls per transition,
+    // so we need to ignore the second one. This might be linked to the use
+    // of `nextTimeout()`, which is required to get the correct value for
+    // `height`, based on `&--label-vertical` calculations.
+    if (enterMap.has(element)) {
+      enterMap.delete(element)
+    } else {
+      enterMap.set(element, true)
+      const { width } = getComputedStyle(element)
+      setStyle(element, {
+        width,
+        position: 'absolute',
+        visibility: 'hidden',
+        height: 'auto'
+      })
+      nextTimeout(() => {
+        const { height } = getComputedStyle(element)
+        setStyle(element, {
+          width: null,
+          position: null,
+          visibility: null,
+          height: 0
+        })
+        forceRepaint(element)
+        nextTimeout(() => setStyle(element, { height }))
+      })
+    }
   },
 
   onLeave(element) {
     const { height } = getComputedStyle(element)
     setStyle(element, { height })
     forceRepaint(element)
-    setTimeout(() => setStyle(element, { height: 0 }))
+    nextTimeout(() => setStyle(element, { height: 0 }))
   }
 }
 </script>
