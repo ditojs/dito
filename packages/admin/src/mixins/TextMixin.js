bcf66b268fe41164bed0913f7f6d616e56734071
Jürg Lehni <juerg@scratchdisk.com>
2023-05-09 18:13:26 +0200

    Move schema.trim back to processValue()

--- packages/admin/src/mixins/TextMixin.js
+++ packages/admin/src/mixins/TextMixin.js
@@ -1,3 +1,4 @@
+import { isString } from '@ditojs/utils'
 import { getSchemaAccessor } from '../utils/accessor.js'
 
 // @vue/component
@@ -9,7 +10,12 @@ export default {
     })
   },
 
-  parseValue(schema, value) {
-    return schema.trim && value != null ? value.trim() : value
+  processValue(schema, value) {
+    if (schema.trim && value != null && isString(value)) {
+      // Text fields don't necessarily have a `String` value when `format()`
+      // without `parse()` is used.
+      value = value.trim()
+    }
+    return value
   }
 }
